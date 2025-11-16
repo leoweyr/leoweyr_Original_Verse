8f22495882a3e03484d199cddef999d4633845d0
leoweyr <contact@leoweyr.com>
2025-11-16 17:02:04 +0800

    feat: add multiple impressions and expressions with random display

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,33 @@ import { Component, type ReactNode } from "react";
 import Background from "./components/Background.tsx";
 import Impression from "./components/Impression.tsx";
 import PerfectIntegrationImage from "./assets/expression/perfect-integration.png";
+import WangMiaoCatImage from "./assets/expression/wang-miao-cat.png";
+import TheEnigmaticDaisyImage from "./assets/expression/the-enigmatic-daisy.png";
+import DeeponlyTaImage from "./assets/expression/deeponly-ta.png";
 import Expression from "./components/Expression.tsx";
 
 
 class App extends Component {
     render(): ReactNode {
+        const impressions: Array<string> = [
+            PerfectIntegrationImage,
+            WangMiaoCatImage,
+            TheEnigmaticDaisyImage,
+            DeeponlyTaImage
+        ];
+        const expressions: Array<string> = [
+            "炜翼",
+            "leoweyr"
+        ];
+
+        const randomImpression: string = impressions[Math.floor(Math.random() * impressions.length)];
+        const randomExpression: string = expressions[Math.floor(Math.random() * expressions.length)];
+
         return (
             <div>
                 <Background />
-                <Impression imageSource={PerfectIntegrationImage}/>
-                <Expression sign={"leoweyr"} />
+                <Impression imageSource={randomImpression}/>
+                <Expression sign={randomExpression} />
             </div>
         );
     }
